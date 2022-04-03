c5b70ce5854e43f04a0d00dccd9e6205f18e8b9a
Алиса Аниськина <alisa.aniskina@gmail.com>
2022-04-03 12:33:52 +0600

    Создание проекта

---
+++ src/main/java/filmcatalog/filmcatalog/FileCatalogApplication.java
@@ -0,0 +1,13 @@
+package filmcatalog.filmcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/filmcatalog/filmcatalog/FileCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package filmcatalog.filmcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
